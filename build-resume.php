cd93f715ffda638a7b30355e6619cd109fa54153
Emmanuel <popoolakenny07@gmail.com>
2023-09-24 18:26:09 +0000

    updated boxicon style link

--- build-resume.php
+++ build-resume.php
@@ -1,17 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <link rel="icon" href="/favicon.ico" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Build Resume</title>
-    <script type="module" crossorigin src="/jobportal/assets/index-5a357dc7.js"></script>
-    <link rel="stylesheet" href="/jobportal/assets/index-4067c918.css">
-    <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
-  </head>
-  <body>
-    <div id="app"></div>
-    
-  </body>
-</html>
 
+<head>
+  <meta charset="UTF-8" />
+  <link rel="icon" href="/favicon.ico" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Build Resume</title>
+  <script type="module" crossorigin src="/jobportal/assets/index-5a357dc7.js"></script>
+  <link rel="stylesheet" href="/jobportal/assets/index-4067c918.css">
+  <!-- Boxicons CSS -->
+  <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+  <!-- <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script> -->
+</head>
+
+<body>
+  <div id="app"></div>
+
+</body>
+
+</html>
